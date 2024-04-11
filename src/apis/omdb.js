c655b495bf375587e89f2379ad5e9696d2fbf8ff
Lakshay Shahi <lakshay.ds@gmail.com>
2024-04-11 13:13:55 +0530

    Updated api url to https

--- src/apis/omdb.js
+++ src/apis/omdb.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 // it will return the api link -
 export function searchMovies(term) {
-    return `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${term}&page=1`;
+    return `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&s=${term}&page=1`;
 }
 
 export function searchMovieById(id) {
-    return `http://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${id}`;
+    return `https://www.omdbapi.com/?apikey=${import.meta.env.VITE_API_KEY}&i=${id}`;
 }
